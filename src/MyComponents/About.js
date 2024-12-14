a42c4258ea9bb93f62020ada76eb30e3c4b1d43b
kash-1308 <kashishsahu1308@gmail.com>
2024-12-15 00:11:55 +0530

    first commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div className="container my-3">
+        <p>Welcome to ToDoList,    your ultimate tool for managing tasks and staying organized!</p>
+    </div>
+  )
+}
